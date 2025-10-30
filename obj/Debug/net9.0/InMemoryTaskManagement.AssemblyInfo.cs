5fc184b33021d46c171b4d1bbace4b0002132589
Hareesh Patil <23amtics452@gmail.com>
2025-10-30 21:15:42 +0530

    Resolved merge conflicts

--- obj/Debug/net9.0/InMemoryTaskManagement.AssemblyInfo.cs
+++ obj/Debug/net9.0/InMemoryTaskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InMemoryTaskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+915c05c5bf23038ff4ebadd0433fa20b995da891")]
 [assembly: System.Reflection.AssemblyProductAttribute("InMemoryTaskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InMemoryTaskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
